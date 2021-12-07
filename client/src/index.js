9e448b46e60841c528facb26b868f933e1bbb8e0
Reed Turgeon <turgeon.dev@gmail.com>
2021-12-06 20:01:04 -0700

    Initial Reacy Query Setup

--- client/src/index.js
+++ client/src/index.js
@@ -1,10 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+
+// components
 import App from './App';
+
+// rQuery
+import { QueryClientProvider } from 'react-query'
+import { ReactQueryDevtools } from "react-query/devtools";
+import { queryClient } from './rQuery_provider'
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <QueryClientProvider client={queryClient}>
+        <App />
+        <ReactQueryDevtools initialIsOpen={false} />
+    </QueryClientProvider>
   </React.StrictMode>,
   document.getElementById('root')
 );

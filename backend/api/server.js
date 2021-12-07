2202ba5f449a5b3ec6c7f6aee6502e7048730297
Reed Turgeon <turgeon.dev@gmail.com>
2021-12-07 02:20:34 -0700

    cleaned up BE routes: added cors & try catch

--- backend/api/server.js
+++ backend/api/server.js
@@ -1,5 +1,6 @@
 // import packages
 const express = require('express')
+const cors = require('cors')
 
 // import db
 const db = require('../db/index')
@@ -8,6 +9,7 @@ const db = require('../db/index')
 const server = express()
 // configure server
 server.use(express.json())
+server.use(cors())
 
 // configure routing
 
@@ -17,27 +19,51 @@ server.get('/', async(req,res) => {
   res.json({ status: "SUCCESS", message: "React Query Demo Server Running" })
 })
 
+// GET
 server.get('/users', async (req, res) => {
-  console.log('TRYING TO GET USERS')
-  const users = await db
-    .select().from('users')
-  res.json(users)
+  try {
+    const users = await db
+      .select().from('users')
+    res.json(users)
+  } catch (err) {
+    res.json({"error": err}) 
+  }
+})
+server.get('/userById', async (req, res) => {
+  const userId = req.query.userId
+  try {
+    const user = await db('users')
+      .select()
+      .from('users')
+      .where('id', userId)
+    res.json(user[0])
+  } catch (err) {
+    res.json({"error": err}) 
+  }
 })
 
+// POST
 server.post('/users', async (req, res) => {
-  console.log('THE BODY NAME', req.body)
-  const user = await db('users')
-    .insert({ name: req.body.name })
-    .returning('*')
-  res.json(user)
+  try {
+    const user = await db('users')
+      .insert({ name: req.body.name })
+      .returning('*')
+    res.json(user)
+  } catch (err) {
+    res.json({"error": err}) 
+  }
 })
 
+// DELETE
 server.delete('/users', async (req, res) => {
-  console.log('THE BODY NAME', req.body)
-  const user = await db('users')
-    .where('name', req.body.name)
-    .del()
-  res.json("success")
+  try {
+    await db('users')
+      .where('name', req.body.name)
+      .del()
+    res.json("success")
+  } catch (err) {
+    res.json({"error": err}) 
+  }
 })
 
 // exports
